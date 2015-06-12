ad904a45c9c1ea8e3b461025b522ee2f9f138d04
bartek96 <github@kt-technik.pl>
2015-06-12 15:11:24 +0200

    second ver

--- vmpc.h
+++ vmpc.h
@@ -1,33 +1,29 @@
-#ifndef _VMPC_H
-#define _VMPC_H
-
-#ifndef uint8
-#define uint8  unsigned char
-#endif
-
-#ifndef uint32
-#define uint32 unsigned long int
-#endif
+#ifndef _VMPC_H_
+#define _VMPC_H_
 
 typedef struct
 {
-	unsigned char P[256];	// Tablica permutacji
+	//----------- VMPC Stream Cipher variables: -----------
+	unsigned char P[256];
 	unsigned char s, n;
-	unsigned char ivInited;	// Potem wyt³umaczê, o co chodzi
-}VmpcState;
+	//----------- VMPC-MAC Authenticated Encryption Scheme variables: -----------
+	unsigned char MAC[32];
+	unsigned char m1, m2, m3, m4, mn;
+	// bonus
+	unsigned char ivInited;
+}VmpcContext;
 
 
-void VMPCInitKeyRound(unsigned char Data[], unsigned char Len, unsigned char Src , VmpcState *s);
-void VMPCInitKeyRound16(unsigned char Data[], unsigned char Src);   //Dla kluczy i wektorow o rozmiarze 16 bajtow (128 bitow)
-void VMPCInitKey(unsigned char Key[], unsigned char Vec[], unsigned char KeyLen, unsigned char VecLen);   //KeyLen, VecLen = 1,2,3,...,64
-void VMPCInitKey16(unsigned char Key[], unsigned char Vec[]);   //Dla kluczy i wektorow o rozmiarze 16 bajtow (128 bitow)
-void VMPCInitKeyBASIC(unsigned char Key[], unsigned char Vec[], unsigned char KeyLen, unsigned char VecLen);   //KeyLen, VecLen = 1,2,3,...,64
-void VMPCInitKey16BASIC(unsigned char Key[], unsigned char Vec[]);   //Dla kluczy i wektorow o rozmiarze 16 bajtow (128 bitow)
-void VMPCEncrypt(unsigned char *Data, unsigned int Len , VmpcState *s);
-void VMPCInitMAC();
-void VMPCEncryptMAC(unsigned char Data[], unsigned int Len , VmpcState *s);
-void VMPCDecryptMAC(unsigned char Data[], unsigned int Len);
-void VMPCOutputMAC();
-void VMPCEraseKey();
+void VmpcInitKeyRound(unsigned char *data, unsigned char len, unsigned char firstInit , VmpcContext *vc);
+void VmpcInitKey(unsigned char *key, unsigned char keyLen, unsigned char *iv, unsigned char ivLen , VmpcContext *vc);
+void VmpcInitKeyBASIC(unsigned char *key, unsigned char keyLen, unsigned char *iv, unsigned char ivLen , VmpcContext *vc);
+void VmpcEncrypt(unsigned char *data, unsigned int len , VmpcContext *vc);
+void VmpcInitMAC(VmpcContext *vc);
+void VmpcEncryptMAC(unsigned char *data, unsigned int len , VmpcContext *vc);
+void VmpcDecryptMAC(unsigned char *data, unsigned int len , VmpcContext *vc);
+void VmpcOutputMAC(VmpcContext *vc);
+void VmpcEraseKey(VmpcContext *vc);
 
-#endif
+
+
+#endif //_VMPC_H_
